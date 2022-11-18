a99176beb19fcf34845796c3de517238c7d5a2d2
Calum Dingwall <29152895+caburum@users.noreply.github.com>
2022-11-18 11:36:34 -0500

    fix multiple bullet levels

--- modules/format unordered lists.ts
+++ modules/format unordered lists.ts
@@ -1,5 +1,5 @@
 export default function(text: string) {
-	return text.replace(/^\*+([^\s\*].*)$/gm, '* $1')
+	return text.replace(/^(\*+)([^\s\*].*)$/gm, '$1 $2')
 }
 
 export let summary = 'formatting lists';
